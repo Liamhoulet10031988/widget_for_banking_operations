c38c133ebfe35229dd5fde5b41a54c06a69f5b7e
Liamhoulet10031988 <liam10031988@gmail.com>
2026-04-19 15:25:18 +0300

    final changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 
 def main() -> None:
     """Точка входа для проверки работы функций."""
-    print(get_mask_card_number(7000792289606361))
-    print(get_mask_account(73654108430135874305))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(get_date("2024-03-11T02:26:18.671407"))
 
 
 if __name__ == "__main__":
--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,28 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(payment_details: str) -> str:
-    """Функция множественной обработыки платежных данных"""
-    pass
+    """Маскирует номер карты или счета в строке с платежными данными."""
+    parts = payment_details.split()
+
+    payment_number = parts[-1]
+    payment_name = " ".join(parts[:-1])
+
+    if len(payment_number) == 16:
+        masked_number = get_mask_card_number(int(payment_number))
+    elif len(payment_number) == 20:
+        masked_number = get_mask_account(int(payment_number))
+    else:
+        return "Некорректный формат номера"
+
+    return f"{payment_name} {masked_number}"
 
 
 def get_date(date: str) -> str:
-    """Функция форматирования даты"""
-    pass
+    """Преобразует дату в формат ДД.ММ.ГГГГ."""
+    result_date = date[0:10]
+    result_final = result_date.split("-")
+    result_final.reverse()
+    result_date = ".".join(result_final)
+
+    return result_date
