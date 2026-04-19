6d25be4b46834d58ced210182c388760f4ebed94
Liamhoulet10031988 <liam10031988@gmail.com>
2026-04-19 14:10:51 +0300

    creating functions

---
+++ src/widget.py
@@ -0,0 +1,8 @@
+def mask_account_card(payment_details: str) -> str:
+    """Функция множественной обработыки платежных данных"""
+    pass
+
+
+def get_date(date: str) -> str:
+    """Функция форматирования даты"""
+    pass
